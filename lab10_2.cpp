8ac1e6fc83a7eb9d565d78b4de39beb6c39919f6
chalammm <tipanan_ji@cmu.ac.th>
2021-12-14 16:50:51 +0700

    completed Lab10-2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,24 +1,39 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO (int n,int m){
+if(n<=0){
+    cout << "Invalid input" << "\n";}
+for (int i = 1; i <= n; i++){
+
+    for (int i = 1; i <= m; i++){
+        cout << "O";
+    }
+    if(m<=0){
+        cout << "Invalid input";break;}
+        cout << "\n";
+    }return 0;
+}
+
+
+
 
 int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+
+    printO(2,2);
+    cout << "\n";
+
+    printO(3,5);
+    cout << "\n";
+
+    printO(5,3);
+    cout << "\n";
+
+    printO(0,3);
+    cout << "\n";
+
+    printO(7,-1);
+    cout << "\n";
+
+    return 0;
 }
